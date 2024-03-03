c2efd6dca44ea0b619bea6a6d8d99126bb8ca0a2
dhruv-gour07 <161121431+dhruv-gour07@users.noreply.github.com>
2024-03-03 20:00:48 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -225,7 +225,7 @@ export default function Contact() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center mt-4 rounded-md bg-slate-700 px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-950 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center mt-4 mb-4 rounded-md bg-slate-700 px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-950 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   {buttonDisabled ? "Fill details properly" : "Send"}
                 </button>
